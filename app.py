e50411367ac4261d3642c9a391fc312aebc7f4bc
Manoj7413 <kumharmanoj303@gmail.com>
2024-08-25 17:14:42 +0530

    Snacke aa gaya

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Project2(Currency Converter).html')
+    return render_template('SnakeGame.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
